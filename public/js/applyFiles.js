31a99711e5bd6b781d99eb927a71ca9c01654f12
Matias Sosa <matiassosas@gmail.com>
2017-11-13 12:37:58 -0300

    Refactor

--- public/js/applyFiles.js
+++ public/js/applyFiles.js
@@ -1,4 +1,6 @@
 $(document).ready(function() { 
+	htmlErrMsg = '<div class="alert alert-dismissible alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><strong><p>Oops..!</p></strong> <p>Something went wrong...</p><span id="error"></span></div>';
+  htmlScsMsg =  '<div class="alert alert-dismissible alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong><p>Local environment updated</p></strong> <p>Your data base is up-to-date</p></div>';
 	$('#loadingDiv').hide();
 
   $("#update-button").on("click", function() { 
@@ -6,10 +8,11 @@ $(document).ready(function() {
   	if ($("#sp-toggle").prop("checked")) {
   		$.get( "http://" + SERVER + ":" + NODEPORT + "/update_from_local", function(data) { 
 	 	 	if(data.code != undefined) {
-	 	   	$("#myTabContent").prepend('<div class="alert alert-dismissible alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><strong><p>Oops..!</p></strong> <p>Something went wrong...</p> Error: ' + data.code + '</div>');	
+	 	   	$("#myTabContent").prepend(htmlErrMsg);
+	 	   	$("#error").prepend("Error: " + data.code);
 	 	 	}
 	 	 	else {
-	 	 		$("#myTabContent").prepend('<div class="alert alert-dismissible alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong><p>Local environment updated</p></strong> <p>Your data base is up-to-date</p></div>');
+	 	 		$("#myTabContent").prepend(htmlScsMsg);
 	 	 	}
 	 	 	$('#loadingDiv').hide();
 	  	});
@@ -18,10 +21,11 @@ $(document).ready(function() {
   		var fileName = $("#file-name").val();
   		$.post( "http://" + SERVER + ":" + NODEPORT + "/run_revision", { file : fileName },function(data) {
   			if(data.code != undefined) {
-	 	   		$("#myTabContent").prepend('<div class="alert alert-dismissible alert-danger"><button type="button" class="close" data-dismiss="alert">×</button><strong><p>Oops..!</p></strong> <p>Something went wrong...</p> Error: ' + data.code + '</div>');	
+	 	   		$("#myTabContent").prepend(htmlErrMsg);
+	 	   		$("#error").prepend("Error: " + data.code);
 	 	 		}
 	 	 		else {
-	 	 			$("#myTabContent").prepend('<div class="alert alert-dismissible alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong><p>Local environment updated</p></strong> <p>Your data base is up-to-date</p></div>');
+	 	 			$("#myTabContent").prepend(htmlScsMsg);
 	 	 		}
 	 	 		$('#loadingDiv').hide();
   		});
